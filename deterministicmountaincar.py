445cd7cf85002238949e5abbc4cfc27c320904b5
Josh Joseph <josh.m.joseph@gmail.com>
2012-09-08 16:17:17 -0400

    mountaincar slip on xdot, improved hill climb

--- deterministicmountaincar.py
+++ deterministicmountaincar.py
@@ -61,63 +61,17 @@ class Mountaincar(rl_tools.Domain):
         x = s[0]
         xdot = s[1]
 
-        if 1:
-            if self.noise[1]:
-                s[0] = min(max(x+xdot + np.random.normal(loc=0, scale=self.noise[1]), self.bounds[0,0]), self.bounds[1,0])
-            else:
-                s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            slip = 0
-            if (np.sign(x-.25) != np.sign(s[0]-.25)) or (np.sign(x-.75) != np.sign(s[0]-.75)): # rocks at -.25, .25, .75
-                if xdot > 0:
-                    slip = max(-self.noise[0], -xdot)
-                else:
-                    slip = min(self.noise[0], -xdot)
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
-        elif 0:
-            slip = 0
-            if .25 < x < .5:
-                if xdot > 0:
-                    slip = max(-self.noise[0]*xdot, -xdot)
-            if self.noise[1]:
-                s[0] = min(max(x+xdot + np.random.normal(loc=0, scale=self.noise[1]), self.bounds[0,0]), self.bounds[1,0])
-            else:
-                s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
-        elif 0:
-            #noise on x
-            if self.noise[1]:
-                s[0] = min(max(x+xdot + self.noise[0] + np.random.normal(loc=0, scale=self.noise[1]), self.bounds[0,0]), self.bounds[1,0])
-            else:
-                s[0] = min(max(x+xdot + self.noise[0], self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)), self.bounds[0,1]), self.bounds[1,1])
-        elif 0:
-            #noise on xdot
+        if self.noise[1]:
+            s[0] = min(max(x+xdot + np.random.normal(loc=0, scale=self.noise[1]), self.bounds[0,0]), self.bounds[1,0])
+        else:
             s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            if self.noise[1]:
-                s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + self.noise[0] + np.random.normal(loc=0, scale=self.noise[1]), self.bounds[0,1]), self.bounds[1,1])
+        slip = 0
+        if (np.sign(x-.25) != np.sign(s[0]-.25)) or (np.sign(x-.75) != np.sign(s[0]-.75)): # rocks at -.25, .25, .75
+            if xdot > 0:
+                slip = max(-self.noise[0], -xdot)
             else:
-                s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + self.noise[0], self.bounds[0,1]), self.bounds[1,1])
-        elif 0:
-            #noise and slip on x
-            slip = 0
-            if -.5 < x < -.75:
-                if xdot > 0:
-                    slip = -self.noise[0]
-                else:
-                    slip = self.noise[0]
-            if self.noise[1]:
-                s[0] = min(max(x+xdot + slip + np.random.normal(loc=0, scale=self.noise[1]), self.bounds[0,0]), self.bounds[1,0])
-            else:
-                s[0] = min(max(x+xdot + slip, self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)), self.bounds[0,1]), self.bounds[1,1])
-        elif 0:
-            #noise and constant slip on xdot
-            slip = 0 if x < -0.5235987755982988 else -np.sign(xdot)*self.noise[0]
-            s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            if self.noise[1]:
-                s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip + np.random.normal(loc=0, scale=self.noise[1]), self.bounds[0,1]), self.bounds[1,1])
-            else:
-                s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
+                slip = min(self.noise[0], -xdot)
+        s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
 
         return s
 
@@ -129,91 +83,21 @@ class Mountaincar(rl_tools.Domain):
         xdot = s[1]
         pmf = np.zeros(self.state_centers.shape[0])
 
-        if 1:
-            s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            slip = 0
-            if x < .25 <= s[0]:
-                if xdot > 0:
-                    slip = max(-self.noise[0], -xdot)
-                else:
-                    slip = min(self.noise[0], -xdot)
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
-            s_next_i = rl_tools.find_nearest_index_fast(self.dim_centers, s)
-            if self.noise[1]:
-                supported_s = self.state_centers[:,1] == self.state_centers[s_next_i,1]
-                tmp_pmf = scipy.stats.norm.pdf(self.state_centers[supported_s,0], loc=s[0], scale=self.noise[1])
-                pmf[supported_s] = tmp_pmf
-                pmf /= np.sum(pmf)
+        s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
+        slip = 0
+        if x < .25 <= s[0]:
+            if xdot > 0:
+                slip = max(-self.noise[0], -xdot)
             else:
-                pmf[s_next_i] = 1.0
-        elif 0:
-            slip = 0 if x < .25 else -np.sign(xdot)*self.noise[0]
-            s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
-            s_next_i = rl_tools.find_nearest_index_fast(self.dim_centers, s)
-            if self.noise[1]:
-                supported_s = self.state_centers[:,1] == self.state_centers[s_next_i,1]
-                tmp_pmf = scipy.stats.norm.pdf(self.state_centers[supported_s,0], loc=s[0], scale=self.noise[1])
-                pmf[supported_s] = tmp_pmf
-                pmf /= np.sum(pmf)
-            else:
-                pmf[s_next_i] = 1.0
-        elif 0:
-            #noise on x
-            s[0] = min(max(x+xdot + self.noise[0], self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)), self.bounds[0,1]), self.bounds[1,1])
-            s_next_i = rl_tools.find_nearest_index_fast(self.dim_centers, s)
-            if self.noise[1]:
-                supported_s = self.state_centers[:,1] == self.state_centers[s_next_i,1]
-                tmp_pmf = scipy.stats.norm.pdf(self.state_centers[supported_s,0], loc=s[0], scale=self.noise[1])
-                pmf[supported_s] = tmp_pmf
-                pmf /= np.sum(pmf)
-            else:
-                pmf[s_next_i] = 1.0
-        elif 0:
-            #noise on xdot
-            s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + self.noise[0], self.bounds[0,1]), self.bounds[1,1])
-            s_next_i = rl_tools.find_nearest_index_fast(self.dim_centers, s)
-            if self.noise[1]:
-                supported_s = self.state_centers[:,0] == self.state_centers[s_next_i,0]
-                tmp_pmf = scipy.stats.norm.pdf(self.state_centers[supported_s,1], loc=s[1], scale=self.noise[1])
-                pmf[supported_s] = tmp_pmf
-                pmf /= np.sum(pmf)
-            else:
-                pmf[s_next_i] = 1.0
-        elif 0:
-            #noise and slip on x
-            slip = 0 if x < -0.5235987755982988 else self.noise[0]*(self.true_pars[0]*np.cos(self.true_pars[1]*x))
-            s[0] = min(max(x+xdot + slip, self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)), self.bounds[0,1]), self.bounds[1,1])
-            s_next_i = rl_tools.find_nearest_index_fast(self.dim_centers, s)
-            if self.noise[1]:
-                supported_s = self.state_centers[:,1] == self.state_centers[s_next_i,1]
-                tmp_pmf = scipy.stats.norm.pdf(self.state_centers[supported_s,0], loc=s[0], scale=self.noise[1])
-                pmf[supported_s] = tmp_pmf
-                pmf /= np.sum(pmf)
-            else:
-                pmf[s_next_i] = 1.0
-        elif 0:
-            #noise and constant slip on xdot
-            slip = 0 if x < -0.5235987755982988 else -np.sign(xdot)*self.noise[0]
-            s[0] = min(max(x+xdot, self.bounds[0,0]), self.bounds[1,0])
-            s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
-            s_next_i = rl_tools.find_nearest_index_fast(self.dim_centers, s)
-            if self.noise[1]:
-                supported_s = self.state_centers[:,0] == self.state_centers[s_next_i,0]
-                tmp_pmf = scipy.stats.norm.pdf(self.state_centers[supported_s,1], loc=s[1], scale=self.noise[1])
-                pmf[supported_s] = tmp_pmf
-                pmf /= np.sum(pmf)
-            else:
-                pmf[s_next_i] = 1.0
+                slip = min(self.noise[0], -xdot)
+        s[1] = min(max(xdot+0.001*u+(self.true_pars[0]*np.cos(self.true_pars[1]*x)) + slip, self.bounds[0,1]), self.bounds[1,1])
+        s_next_i = rl_tools.find_nearest_index_fast(self.dim_centers, s)
+        if self.noise[1]:
+            supported_s = self.state_centers[:,1] == self.state_centers[s_next_i,1]
+            tmp_pmf = scipy.stats.norm.pdf(self.state_centers[supported_s,0], loc=s[0], scale=self.noise[1])
+            pmf[supported_s] = tmp_pmf
+            pmf /= np.sum(pmf)
+        else:
+            pmf[s_next_i] = 1.0
 
         return pmf
-
-#print "[mountaincar]: only noise on x !!!!!!!!!!!!!!"
-#print "[mountaincar]: only noise on xdot !!!!!!!!!!!!!!"
-#print "[mountaincar]: noise and slip on x !!!!!!!!!!!!!!"
-#print "[mountaincar]: noise and constant slip on xdot !!!!!!!!!!!!!!"
-#print "[mountaincar]: noise on x, slip on xdot !!!!!!!!!!!!!!"
-
